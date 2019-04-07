68b80d48e082b22bab2595d2b13cba25272a6aef
tuengominh <tue.ngominh@gmail.com>
2019-04-07 04:15:52 +0200

    Implement find method

--- src/main/java/co/masterbridge/website/Application.java
+++ src/main/java/co/masterbridge/website/Application.java
@@ -1,8 +1,13 @@
 package co.masterbridge.website;
 
+import co.masterbridge.website.model.Course;
+import co.masterbridge.website.repository.CourseRepository;
+import co.masterbridge.website.service.CourseService;
 import co.masterbridge.website.util.DataSourceUtil;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.ScriptUtils;
 
@@ -12,11 +17,20 @@ import java.sql.SQLException;
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
 
         SpringApplication.run(Application.class, args);
 
-        DataSource dataSource = DataSourceUtil.getDataSourceInPath();
-        ScriptUtils.executeSqlScript(dataSource.getConnection(), new ClassPathResource("sql-scripts/university_schema.sql"));
+        //DataSource dataSource = DataSourceUtil.getDataSourceInPath();
+        //ScriptUtils.executeSqlScript(dataSource.getConnection(), new ClassPathResource("sql-scripts/university_schema.sql"));
+    }
+
+    @Bean
+    public CommandLineRunner test(
+            CourseService courseService,
+            CourseRepository courseRepository) {
+
+        return args -> {
+        };
     }
 }
