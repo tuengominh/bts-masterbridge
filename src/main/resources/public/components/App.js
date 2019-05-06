51ac1fc891d35266fa7fda14bd54dad81994b381
tuengominh <tue.ngominh@gmail.com>
2019-05-06 16:09:56 +0200

    create index.html loading App react component

---
+++ src/main/resources/public/components/App.js
@@ -0,0 +1,18 @@
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+    }
+
+    render() {
+        return (
+            <div>
+                <div><h1>Opening minds, shaping futures</h1></div>
+                <div><h2>Your career built upon your potential</h2></div>
+            </div>
+        );
+    }
+}
