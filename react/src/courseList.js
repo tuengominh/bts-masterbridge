fd72c18f0d61c236f48f8db2ef532fa55d46a052
tuengominh <tue.ngominh@gmail.com>
2019-05-31 09:27:51 +0200

    Add font awesome + google fonts

--- react/src/courseList.js
+++ react/src/courseList.js
@@ -9,12 +9,12 @@ class CourseList extends Component {
             <div>
                 <h1>This is the course list.</h1>
                 <div>
-                    <Link to="/course">Course 1</Link>
+                    <Link to="/course/5cd8687c1c9d440000d7f7a3">Course 1</Link>
                 </div>
             </div>
         );
     }
-
 }
 
 export default CourseList;
+
